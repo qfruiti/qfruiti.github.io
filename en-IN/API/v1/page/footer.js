d39501533b0f34f745cf727dcae82dd27a0cafa6
rimthekid <cppoxking@gmail.com>
2020-10-26 18:15:32 +0330

    kkk

---
+++ en-IN/API/v1/page/footer.js
@@ -0,0 +1,174 @@
+{
+	"title": "Footer",
+	"menuLink1": [{
+		"actionText": "CONTACT US",
+		"actionCssClass": "menu1Class",
+		"isExternalUrl": "No",
+		"actionLinkUrl": "/en-IN/contactus",
+		"actionTargetType": "_self",
+		"actionType": "link"
+	}],
+	"menuLink2": [{
+		"actionText": "Privacy Policy",
+		"actionCssClass": "menu2Class",
+		"isExternalUrl": "No",
+		"actionLinkUrl": "/en-IN/privacypolicy",
+		"actionTargetType": "_self",
+		"actionType": "link"
+	}, {
+		"actionText": "Terms and Conditions",
+		"actionCssClass": "menu2Class",
+		"isExternalUrl": "No",
+		"actionLinkUrl": "/en-IN/termsandconditions",
+		"actionTargetType": "_self",
+		"actionType": "link"
+	}],
+	"copyrightText": {
+		"title": "Copyright",
+		"labelText": "©Zespri Group Limited, all rights reserved"
+	},
+	"regions": [{
+		"regionTitle": "Europe",
+		"countryList": [{
+			"title": "Belgium (Dutch)",
+			"url": "/nl-BE",
+			"locale": "nl-be"
+		}, {
+			"title": "Belgium (French)",
+			"url": "/fr-BE",
+			"locale": "fr-be"
+		}, {
+			"title": "France",
+			"url": "/fr-FR",
+			"locale": "fr-FR"
+		},{
+			"title": "Germany",
+			"url": "/de-DE",
+			"locale": "de-de"
+		}, {
+			"title": "Greece",
+			"url": "/el-GR",
+			"locale": "el-gr"
+		}, {
+			"title": "Italy",
+			"url": "/it-IT",
+			"locale": "it-it"
+		}, {
+			"title": "Luxembourg",
+			"url": "/fr-LU",
+			"locale": "fr-LU"
+		}, {
+			"title": "Netherlands",
+			"url": "/nl-NL",
+			"locale": "nl-nl"
+		}, {
+			"title": "Norway",
+			"url": "/nb-NO",
+			"locale": "nb-no"
+		}, {
+			"title": "Portugal",
+			"url": "/pt-PT",
+			"locale": "pt-pt"
+		}, {
+			"title": "Spain",
+			"url": "/es-ES",
+			"locale": "es-es"
+		}, {
+			"title": "Sweden",
+			"url": "/sv-SE",
+			"locale": "sv-se"
+		}, {
+			"title": "United Kingdom",
+			"url": "/en-UK",
+			"locale": "en-uk"
+		}]
+	}, {
+		"regionTitle": "Greater China ",
+		"countryList": [{
+			"title": "China",
+			"url": "https://www.zespri.com.cn/"
+		}, {
+			"title": "Hong Kong",
+			"url": "/zh-HK",
+			"locale": "zh-hk"
+		}, {
+			"title": "Taiwan",
+			"url": "/zh-TW",
+			"locale": "zh-tw"
+		}]
+	}, {
+		"regionTitle": "India",
+		"countryList": [{
+			"title": "India",
+			"url": "/en-IN",
+			"locale": "en-in"
+		}]
+	}, {
+		"regionTitle": "North America ",
+		"countryList": [{
+			"title": "North America",
+			"url": "/en-US",
+			"locale": "en-us"
+		}, {
+			"title": "Mexico",
+			"url": "/es-MX",
+			"locale": "es-mx"
+		}]
+	}, {
+		"regionTitle": "North Asia",
+		"countryList": [{
+			"title": "Japan",
+			"url": "/ja-JP",
+			"locale": "ja-JP"
+		}, {
+			"title": "Korea",
+			"url": "/ko-KR",
+			"locale": "ko-kr"
+		}]
+	}, {
+		"regionTitle": "Oceania",
+		"countryList": [{
+			"title": "Australia",
+			"url": "/en-AU",
+			"locale": "en-au"
+		}, {
+			"title": "New Zealand ",
+			"url": "/en-NZ",
+			"locale": "en-nz"
+		}]
+	}, {
+		"regionTitle": "South America",
+		"countryList": [{
+			"title": "Brazil",
+			"url": "/pt-BR",
+			"locale": "pt-br"
+		}]
+	}, {
+		"regionTitle": "South East Asia ",
+		"countryList": [{
+			"title": "Vietnam",
+			"url": "/vi-VN",
+			"locale": "vi-vn"
+		}, {
+			"title": "Thailand",
+			"url": "/th-TH",
+			"locale": "th-th"
+		}, {
+			"title": "Philippines",
+			"url": "/en-PH",
+			"locale": "en-ph"
+		}, {
+			"title": "Malaysia",
+			"url": "/en-MY",
+			"locale": "en-my"
+		}, {
+			"title": "Indonesia",
+			"url": "/id-ID",
+			"locale": "id-id"
+		}, {
+			"title": "Singapore",
+			"url": "/en-SG",
+			"locale": "en-sg"
+		}]
+	}]
+}
---
+++ en-IN/API/v1/page/header.js
@@ -0,0 +1,95 @@
+{
+	"title": "Header",
+	"logo": {
+		"title": "Logo",
+		"description": "zespri-logo",
+		"file": {
+			"url": "./images/header/logo.png",
+			"details": {
+				"size": 7782,
+				"image": {
+					"width": 140,
+					"height": 125
+				}
+			},
+			"fileName": "logo.png",
+			"contentType": "image/png"
+		}
+	},
+	"gaScript": "GA Scripts to be added",
+	"primaryMenu": [{
+		"title": "OUR KIWIS",
+		"links": [{
+			"actionText": "OUR KIWIS",
+			"actionCssClass": "menuClass",
+			"isExternalUrl": "No",
+			"actionLinkUrl": "/en-IN/product",
+			"actionTargetType": "_self",
+			"actionType": "link"
+		}]
+	}, {
+		"title": "NUTRITION AND HEALTH",
+		"links": [{
+			"actionText": "NUTRITION & HEALTH",
+			"isExternalUrl": "No",
+			"actionLinkUrl": "/en-IN/nutrition",
+			"actionTargetType": "_self",
+			"actionType": "link"
+		}]
+	}, {
+		"title": "RECIPES",
+		"links": [{
+			"actionText": "RECIPES",
+			"actionCssClass": "menuClass",
+			"isExternalUrl": "No",
+			"actionLinkUrl": "/en-IN/recipes",
+			"actionTargetType": "_self",
+			"actionType": "link"
+		}]
+	}, {
+		"title": "THE ZESPRI BLOG ",
+		"links": [{
+			"actionText": "ALL ABOUT KIWIS",
+			"actionCssClass": "menuClass",
+			"isExternalUrl": "No",
+			"actionLinkUrl": "/en-IN/blog",
+			"actionTargetType": "_self",
+			"actionType": "link"
+		}]
+	}, {
+		"title": "ZESPRI DIFFERENCE",
+		"links": [{
+			"actionText": "ZESPRI DIFFERENCE",
+			"actionCssClass": "menuClass",
+			"isExternalUrl": "No",
+			"actionLinkUrl": "/en-IN/zespri-system",
+			"actionTargetType": "_self",
+			"actionType": "link"
+		}]
+	}, {
+		"title": "",
+		"links": [{
+			"actionText": "",
+			"actionCssClass": "",
+			"isExternalUrl": "No",
+			"actionLinkUrl": "",
+			"actionTargetType": "_self",
+			"actionType": "link"
+		}]
+	}],
+	"secondaryMenu": [{
+		"title": "Search",
+		"className": "show-search",
+		"images": [{
+			"image_video_title": "Back button image",
+			"mediaCssClass": "fa fa-search"
+		}],
+		"links": [{
+			"actionText": "Search",
+			"isExternalUrl": "No",
+			"actionLinkUrl": "/en-IN/search",
+			"actionTargetType": "_self",
+			"actionType": "link"
+		}]
+	}]
+}
