4cc359f4644fcfc2aff358c847bf1d2467969ca3
rimthekid <cppoxking@gmail.com>
2020-10-26 18:04:33 +0330

    kk

---
+++ en-IN/API/v1/page/home.js
@@ -0,0 +1,334 @@
+{
+	"pageTitle": "Home",
+	"pageIdentifier": "/",
+	"seo": {
+		"metaTitle": "Home",
+		"metaDescription": "Welcome to Zespri Kiwifruit. Our Green and SunGold kiwifruit deliver better flavour, higher quality and more vitality in every bite. Discover mouth-watering recipes and taste the difference for yourself.",
+		"metaKeywords": "keyword1, keyword2, keyword3",
+		"metaTags": "tag1, tag2, tag3"
+	},
+	"sections": [{
+		"ZingySection": {
+			"sectionTitle": "",
+			"sectionDescription": "",
+			"sectionCssClass": "SectionClass",
+			"visible": true,
+			"images": [{
+				"image_video_title": "Homepage Banner Images",
+				"imageUrl": "./images/homepage/india_KV_desktop.jpg",
+				"imageMobileUrl": "./images/homepage/india_KV_mobile.jpg"
+
+			}]
+		}
+	}, {
+		"DeliciousSection": {
+			"sectionTitle": "PACKED WITH  <span class='darkgreen-text'> NUTRIENTS</span>",
+			"sectionCssClass": "SectionClass",
+			"visible": true,
+			"images": [{
+				"image_video_title": "zespri-nutrition-masthead",
+				"imageUrl": "./images/homepage/Nutrition_Desktop.jpg",
+				"imageAltText": "nutrition-masthead-mobile-min.jpg",
+				"imageMobileUrl": "./images/homepage/Nutrition_Mobile.jpg"
+			}],
+			"links": [{
+				"actionText": "<button class='darkgreen-bg'>LEARN MORE</button>",
+				"actionCssClass": "NutritionClass",
+				"isExternalUrl": "No",
+				"actionLinkUrl": "/en-IN/nutrition",
+				"actionTargetType": "_self",
+				"actionType": "button"
+			}]
+		}
+	}, {
+		"ProductSection": {
+			"sectionTitle": "Product Listing Section",
+			"sectionCssClass": "SectionClass",
+			"visible": true,
+			"links": [{
+				"actionText": "<span class=yellowLink>Our Products</span>",
+				"actionCssClass": "yellowLink",
+				"isExternalUrl": "No",
+				"actionLinkUrl": "/en-IN/product",
+				"actionTargetType": "_self",
+				"actionType": "link"
+			}, {
+				"actionText": "<span class=greenLink>WHERE TO BUY</span>",
+				"actionCssClass": "greenLink",
+				"isExternalUrl": "No",
+				"actionLinkUrl": "/en-IN/wheretobuy",
+				"actionTargetType": "_self",
+				"actionType": "link"
+			}],
+			"products": [{
+				"productTitle": "<h3 class='yellow-text'>ZESPRI<span class='number'>™</span> SUNGOLD<span class='number'>™</span></h3>",
+				"tagline": "<p>Golden and gloriously sweet.</p>",
+				"images": {
+					"image_video_title": "zespri-sungold-product",
+					"imageUrl": "./images/product/homepage-product-sungold-min.png",
+					"imageAltText": "homepage-product-sungold-min.png",
+					"mediaCssClass": "zespri-sungold-product"
+				},
+				"productUrl": "/en-IN/product"
+			}, {
+				"productTitle": "<h3 class='green-text'>ZESPRI<span class='number'>™</span> GREEN</h3>",
+				"tagline": "<p>Green means go for zest and zing.</p>",
+				"images": {
+					"image_video_title": "zespri-green-product",
+					"imageUrl": "./images/product/homepage-product-green-min.png",
+					"imageAltText": "homepage-product-green-min.png",
+					"mediaCssClass": "zespri-green-product-tilt"
+				},
+				"productUrl": "/en-IN/product"
+			}, {
+				"productTitle": "<h3 class='pink-text'>ZESPRI<span class='number'>™</span>  ORGANIC</h3>",
+				"tagline": "<p>Available in Green and SunGold™.</p>",
+				"images": {
+					"image_video_title": "zespri-organic-product",
+					"imageUrl": "./images/product/homepage-product-organic-min.png",
+					"imageAltText": "homepage-product-organic-min.png",
+					"mediaCssClass": "zespri-organic-product"
+				},
+				"productUrl": "/en-IN/product"
+			}]
+		}
+	}, {
+		"YouCanSection": {
+			"sectionTitle": "YouCanSection",
+			"sectionCssClass": "SectionClass",
+			"visible": true,
+			"slides": [{
+				"title": "<button class='ways-to-eat-button-green'>SCOOP IT</button>",
+				"labelCssClass": "labelCssClass",
+				"images": [{
+					"image_video_title": "Scoop It",
+					"videoUrl": "./images/homepage/scoop_1.mp4"
+				}],
+				"links": [{
+					"actionText": "Scoop_Action",
+					"isExternalUrl": "No",
+					"actionLinkUrl": "/en-IN/blogdetail/can-i-eat-the-skin-of-kiwifruit",
+					"actionTargetType": "_self",
+					"actionType": "link"
+				}]
+			}, {
+				"title": "<button class='ways-to-eat-button-green'>BITE IT</button>",
+				"labelCssClass": "labelCssClass",
+				"images": [{
+					"image_video_title": "Bite It ",
+					"videoUrl": "./images/homepage/bite_1.mp4"
+				}],
+				"links": [{
+					"actionText": "Bite_Action",
+					"isExternalUrl": "No",
+					"actionLinkUrl": "/en-IN/blogdetail/can-i-eat-the-skin-of-kiwifruit",
+					"actionTargetType": "_self",
+					"actionType": "link"
+				}]
+			}, {
+				"title": "<button class='ways-to-eat-button-green'>PEEL IT</button>",
+				"labelCssClass": "labelCssClass",
+				"images": [{
+					"image_video_title": "Peel It",
+					"videoUrl": "./images/homepage/peel_1.mp4"
+				}],
+				"links": [{
+					"actionText": "Peel_Action",
+					"isExternalUrl": "No",
+					"actionLinkUrl": "/en-IN/blogdetail/can-i-eat-the-skin-of-kiwifruit",
+					"actionTargetType": "_self",
+					"actionType": "link"
+				}]
+			}, {
+				"title": "<button class='ways-to-eat-button-green'>SLICE IT</button>",
+				"labelCssClass": "labelCssClass",
+				"images": [{
+					"image_video_title": "Slice It",
+					"videoUrl": "./images/homepage/slice_1.mp4"
+				}],
+				"links": [{
+					"actionText": "Slice_Action",
+					"isExternalUrl": "No",
+					"actionLinkUrl": "/en-IN/blogdetail/can-i-eat-the-skin-of-kiwifruit",
+					"actionTargetType": "_self",
+					"actionType": "link"
+				}]
+			}, {
+				"title": "<button class='ways-to-eat-button-green'>SPOON IT</button>",
+				"labelCssClass": "labelCssClass",
+				"images": [{
+					"image_video_title": "Spoon It",
+					"videoUrl": "./images/homepage/spoon_2_1.mp4"
+				}],
+				"links": [{
+					"actionText": "Spoon_Action",
+					"isExternalUrl": "No",
+					"actionLinkUrl": "/en-IN/blogdetail/can-i-eat-the-skin-of-kiwifruit",
+					"actionTargetType": "_self",
+					"actionType": "link"
+				}]
+			}]
+		}
+	}, {
+		"RecipeSection": {
+			"sectionTitle": "ADD A BIT OF <span class=lightGreenText>ZING</span>",
+			"sectionCssClass": "SectionClass",
+			"visible": true,
+			"links": [{
+				"actionText": "<button class='darkgreen-bg'>GET COOKING</button>",
+				"actionCssClass": "cssClass",
+				"isExternalUrl": "No",
+				"actionLinkUrl": "/en-IN/recipes",
+				"actionTargetType": "_self",
+				"actionType": "button"
+			}],
+			"recipes": [{
+				"recipeTitle": "Kiwi Colada",
+				"slug": "kiwi-colada",
+				"recipeThumbnailImage": {
+					"image_video_title": "kiwi-colada-thumbnail",
+					"imageUrl": "./images/recipe/thumbnail/Kiwicolada-min.jpg",
+					"imageAltText": "Kiwicolada-min.jpg"
+				},
+				"images": {
+					"image_video_title": "kiwi-colada-masthead",
+					"imageUrl": "./images/recipe/masthead/Kiwicolada-DMasthead.jpg",
+					"imageAltText": "kiwicolada-MMasthead.jpg",
+					"imageMobileUrl": "./images/recipe/masthead/kiwicolada-MMasthead.jpg"
+				},
+				"recipeLikes": 0,
+				"likesCssClass": "likesClass",
+				"durationText": "15 mins",
+				"durationCssClass": "durationclass",
+				"servesText": "Serves 4",
+				"servesCssClass": "servesclass",
+				"recipeUrl": "/en-IN/recipedetail/"
+			}, {
+				"recipeTitle": "Kiwifruit & Paneer on the Skewer",
+				"slug": "kiwifruit-and-paneer-on-the-skewer",
+				"recipeThumbnailImage": {
+					"image_video_title": "kiwifruit-and-paneer-on-the-skewer-thumbnail",
+					"imageUrl": "./images/recipe/thumbnail/Kiwifruit-and-Paneer-on-the-skewer-min.jpg",
+					"imageAltText": "Kiwifruit-and-Paneer-on-the-skewer-min.jpg"
+				},
+				"images": {
+					"image_video_title": "kiwifruit-and-paneer-on-the-skewer-masthead",
+					"imageUrl": "./images/recipe/masthead/Kiwifruit-and-Paneer-on-the-skewer-DMasthead.jpg",
+					"imageAltText": "kiwifruit-and-paneer-on-the-skewer-MMasthead.jpg",
+					"imageMobileUrl": "./images/recipe/masthead/kiwifruit-and-paneer-on-the-skewer-MMasthead.jpg"
+				},
+				"recipeLikes": 0,
+				"durationText": "25 mins",
+				"servesText": "Serves 10",
+				"servesCssClass": "servesclass",
+				"recipeUrl": "/en-IN/recipedetail/"
+			}, {
+				"recipeTitle": "Kiwifruit Paanipuri",
+				"slug": "kiwifruit-paanipuri",
+				"recipeThumbnailImage": {
+					"image_video_title": "kiwifruit-paanipuri-thumbnail",
+					"imageUrl": "./images/recipe/thumbnail/kiwifruit-paanipuri-min.jpg",
+					"imageAltText": "kiwifruit-paanipuri-min.jpg"
+				},
+				"images": {
+					"image_video_title": "kiwifruit-paanipuri-masthead",
+					"imageUrl": "./images/recipe/masthead/kiwifruit-paanipuri-DMasthead.jpg",
+					"imageAltText": "kiwifruit-paanipuri-MMasthead.jpg",
+					"imageMobileUrl": "./images/recipe/masthead/kiwifruit-paanipuri-MMasthead.jpg"
+				},
+				"recipeLikes": 0,
+				"durationCssClass": "durationclass",
+				"servesText": "Serves 4",
+				"servesCssClass": "servesclass",
+				"recipeUrl": "/en-IN/recipedetail/"
+			}, {
+				"recipeTitle": "Kiwifruit Papdi Chaat",
+				"slug": "kiwifruit-papdi-chaat",
+				"recipeThumbnailImage": {
+					"image_video_title": "kiwifruit-papdi-chaat-thumbnail",
+					"imageUrl": "./images/recipe/thumbnail/kiwifruit-papdi-chaat-min.jpg",
+					"imageAltText": "kiwifruit-papdi-chaat-min.jpg"
+				},
+				"recipeLikes": 0,
+				"likesCssClass": "likesClass",
+				"durationText": "15 mins",
+				"durationCssClass": "durationclass",
+				"servesText": "Serves 28",
+				"servesCssClass": "servesclass",
+				"recipeUrl": "/en-IN/recipedetail/"
+			}]
+		}
+	}, {
+		"KiwiUniverseSection": {
+			"sectionTitle": "KiwiUniverse Section",
+			"sectionCssClass": "SectionClass",
+			"visible": true,
+			"slides": [{
+				"title": "<span class=greenBackground>Kiwifruit Nutrition</span>",
+				"elementIdentifier": "slide4",
+				"labelCssClass": "labelCssClass",
+				"images": [{
+					"image_video_title": "dna-of-delicious",
+					"imageUrl": "./images/homepage/Footer-right-image_NutritionA.jpg",
+					"imageAltText": "Footer-right-image_NutritionA.jpg"
+				}],
+				"links": [{
+					"actionText": "Kiwifruit Nutrition",
+					"isExternalUrl": "No",
+					"actionLinkUrl": "/en-IN/nutrition",
+					"actionTargetType": "_self",
+					"actionType": "link"
+				}]
+			}]
+		}
+	}, {
+		"FollowUsSection": {
+			"sectionTitle": " Follow Us Section",
+			"visible": true,
+			"followUs": {
+				"followustitle": "FOLLOW US, WE DON'T BITE.",
+				"socialMediaTokens": [{
+					"Facebook": {
+						"images": {
+							"image_video_title": "Facebook Image",
+							"imageUrl": "./images/footer/FB_ICON.png",
+							"imageAltText": "FB ICON.png",
+							"mediaCssClass": "facebookclass"
+						},
+						"redirectUrl": "https://www.facebook.com/ZespriIndia/?fref=ts"
+					}
+				}, {
+					"Instagram": {
+						"images": {
+							"image_video_title": "Instagram Image",
+							"imageUrl": "./images/footer/IG_ICON.png",
+							"imageAltText": "Zespri_Icons-2_10.png",
+							"mediaCssClass": "instagramcssclass"
+						},
+						"redirectUrl": "https://www.instagram.com/zesprikiwifruitindia/"
+					}
+				}, {
+					"Twitter": {
+						"images": {
+							"image_video_title": "Twitter Image",
+							"imageUrl": "./images/footer/TWITTER_ICON.png",
+							"imageAltText": "_D_Zespri-Way_03.png",
+							"mediaCssClass": "twittercssclass"
+						},
+						"redirectUrl": "https://twitter.com/zespriindia"
+					}
+				},  {
+					"YouTube": {
+						"images": {
+							"image_video_title": "YouTube Image",
+							"imageUrl": "./images/footer/YT_ICON.png",
+							"imageAltText": "Zespri_D_Blog-Landing_Slicing_32.png",
+							"mediaCssClass": "youtubecssclass"
+						},
+						"redirectUrl": "https://www.youtube.com/channel/UCXcDWbszKRfzs-KxKuZNBZQ"
+					}
+				}]
+			}
+		}
+	}]
+}
