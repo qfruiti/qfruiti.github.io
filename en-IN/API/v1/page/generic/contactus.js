699cb9f4c3c0ea5661fac5da2efbc41ac43b4f5b
rimthekid <cppoxking@gmail.com>
2021-01-26 11:13:01 +0330

    contactus js

---
+++ en-IN/API/v1/page/generic/contactus.js
@@ -0,0 +1,91 @@
+{
+	"pageTitle": "Contact Zespri India",
+	"pageIdentifier": "/contactus",
+	"seo": {
+		"metaTitle": "Contact Zespri India",
+		"metaDescription": "We love hearing from you. Fill out the contact form below for all Zespri India inquiries. From here, you can also click the link for countries outside India. ",
+		"metaKeywords": "keyword1, keyword2, keyword3",
+		"metaTags": "tag1, tag2, tag3"
+	},
+	"sections": [{
+		"ContactUsSection": {
+			"sectionTitle": "<span class=greenText>Contact Us</span>",
+			"sectionDescription": "We want to hear from you!",
+			"sectionCssClass": "SectionClass",
+			"visible": true,
+			"contactUs": {
+				"feedbackTitle": "<span>FOR INQUIRIES and FEEDBACK</span>",
+				"tabOne": "<span>CONSUMER INQUIRIES AND FEEDBACK</span>",
+				"tabTwo": "<span>BUSINESS INQUIRIES <div>- Reach out to us if you are a wholesaler, distributor, or store owner</div></span>",
+				"firstName": "First Name*",
+				"lastName": "Last Name*",
+				"emailAddress": "Email Address*",
+				"telephone": "Telephone*",
+				"message": "Your Message*",
+				"checkboxOne": "I have read and agree to the <a href='/en-IN/termsandconditions'>Terms and conditions</a>  and  <a href='/en-IN/privacypolicy'>Privacy Policy</a>\n\n",
+				"checkboxTwo": "Subscribe to updates on Zespri Kiwifruit",
+				"links": {
+					"actionText": "<span class=yellowBackground>SUBMIT</span>",
+					"actionCssClass": "yellowBackground",
+					"actionTargetType": "_self",
+					"actionType": "button"
+				},
+				"submitMessage": "Thank You!",
+				"mailLabel": "<span class=lightgreenBackground>REACH US BY MAIL</span>",
+				"mailDetails": "<span class=greenText><b>Zespri International (India) Pvt. Ltd.</b></span><br>5-109, WeWork, 4th Floor, Building 4, North Wing <br>NESCO IT Park, Western Express Highway, Goregaon (East)Mumbai â€“ 4000 063, Maharashtra, India<br><b>Consumer Enquiries: <a href='mailto:akshay.pai@zespri.com?Subject=Contact Us' target='_top'><span class='lightGreenText'>akshay.pai@zespri.com</span></a><br>Business Enquiries: <a href='mailto:mohit.jain@zespri.com?Subject=Contact Us' target='_top'><span class='lightGreenText'>mohit.jain@zespri.com</span></a></b>",
+				"mediaEnquiriesLabel": "<span class=lightgreenBackground>MEDIA INQUIRIES  </span>",
+				"mediaEnquiriesDetails": "For media/press inquiries  please write to <b>  <a href='mailto:akshay.pai@zespri.com?Subject=Contact Us' target='_top'><span class='lightGreenText'>akshay.pai@zespri.com</span></a></b>"
+			}
+		}
+	}, {
+		"FollowUsSection": {
+			"sectionTitle": "Follow Us Section",
+			"sectionCssClass": "SectionClass",
+			"visible": true,
+			"followUs": {
+				"followustitle": "FOLLOW US, WE DON'T BITE.",
+				"socialMediaTokens": [{
+					"Facebook": {
+						"images": {
+							"image_video_title": "Facebook Image",
+							"imageUrl": "./images/footer/FB_ICON.png",
+							"imageAltText": "FB ICON.png",
+							"mediaCssClass": "facebookclass"
+						},
+						"redirectUrl": "https://www.facebook.com/ZespriIndia/?fref=ts"
+					}
+				}, {
+					"Instagram": {
+						"images": {
+							"image_video_title": "Instagram Image",
+							"imageUrl": "./images/footer/IG_ICON.png",
+							"imageAltText": "Zespri_Icons-2_10.png",
+							"mediaCssClass": "instagramcssclass"
+						},
+						"redirectUrl": "https://www.instagram.com/zesprikiwifruitindia/"
+					}
+				}, {
+					"Twitter": {
+						"images": {
+							"image_video_title": "Twitter Image",
+							"imageUrl": "./images/footer/TWITTER_ICON.png",
+							"imageAltText": "_D_Zespri-Way_03.png",
+							"mediaCssClass": "twittercssclass"
+						},
+						"redirectUrl": "https://twitter.com/zespriindia"
+					}
+				}, {
+					"YouTube": {
+						"images": {
+							"image_video_title": "YouTube Image",
+							"imageUrl": "./images/footer/YT_ICON.png",
+							"imageAltText": "Zespri_D_Blog-Landing_Slicing_32.png",
+							"mediaCssClass": "youtubecssclass"
+						},
+						"redirectUrl": "https://www.youtube.com/channel/UCXcDWbszKRfzs-KxKuZNBZQ"
+					}
+				}]
+			}
+		}
+	}]
+}
